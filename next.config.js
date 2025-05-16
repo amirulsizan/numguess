01a46709260bef1b26a86d4eb2ae873edb4cc311
Amirul Sizan <67902747+amirulsizan@users.noreply.github.com>
2025-05-16 11:29:59 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,1 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-	reactStrictMode: true,
-	swcMinify: true,
-	images: {
-		remotePatterns: [
-			{
-				protocol: 'https',
-				hostname: '*.aglty.io',
-			},
-		],
-	},
-}
 
-module.exports = nextConfig
